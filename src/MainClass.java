fe6f63239f77315baa62e8b2bb19256c8f46e04d
jiujiuwo <1203574299@qq.com>
2018-11-17 22:18:27 +0800

    problem 3 with java map,time limit exceeded

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,121 +1,43 @@
-/* -----------------------------------
- *  WARNING:
- * -----------------------------------
- *  Your code may fail to compile
- *  because it contains public class
- *  declarations.
- *  To fix this, please remove the
- *  "public" keyword from your class
- *  declarations.
- */
+import java.util.HashMap;
+import java.util.Map;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- * int val;
- * ListNode next;
- * ListNode(int x) { val = x; }
- * }
- */
 class Solution {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode(l1.val + l2.val);
-        ListNode ptr = head;
+    public int lengthOfLongestSubstring(String s) {
 
-        while (l1.next != null || l2.next != null) {
-            if (l1.next != null) {
-                l1 = l1.next;
-            } else {
-                l1.val = 0;
-            }
+        int maxLen = 0;
+        char[] chars = s.toCharArray();
 
-            if (l2.next != null) {
-                l2 = l2.next;
-            } else {
-                l2.val = 0;
-            }
-            ptr.next = new ListNode(l1.val + l2.val);
-            ptr = ptr.next;
-        }
-
-        ptr = head;
-        while (ptr != null) {
-            if (ptr.val >= 10) {
-                ptr.val -= 10;
-                if (ptr.next != null) {
-                    ptr.next.val += 1;
-                } else {
-                    ptr.next = new ListNode(1);
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = i+1; j<= chars.length; j++) {
+                //System.out.println(s.substring(i,j));
+                int tmp = checkDulplicateAndLen(s.substring(i,j));
+                if(tmp > maxLen){
+                    maxLen = tmp;
                 }
             }
-            ptr = ptr.next;
         }
+        return maxLen;
+    }
 
-        return head;
+    //使用map计数的方法解此题，超时
+    private int checkDulplicateAndLen(String substring) {
+        int numChars = 0;
+        Map<Character,Integer> charMap = new HashMap<>();
+        for(int i =0;i < substring.length();i++){
+            if(charMap.containsKey(substring.charAt(i))){
+                return 0;
+            }else {
+                charMap.put(substring.charAt(i),0);
+                numChars++;
+            }
+        }
+        return numChars;
     }
 }
 
 public class MainClass {
-    public static int[] stringToIntegerArray(String input) {
-        input = input.trim();
-        input = input.substring(1, input.length() - 1);
-        if (input.length() == 0) {
-            return new int[0];
-        }
-
-        String[] parts = input.split(",");
-        int[] output = new int[parts.length];
-        for (int index = 0; index < parts.length; index++) {
-            String part = parts[index].trim();
-            output[index] = Integer.parseInt(part);
-        }
-        return output;
-    }
-
-    public static ListNode stringToListNode(String input) {
-        // Generate array from the input
-        int[] nodeValues = stringToIntegerArray(input);
-
-        // Now convert that list into linked list
-        ListNode dummyRoot = new ListNode(0);
-        ListNode ptr = dummyRoot;
-        for (int item : nodeValues) {
-            ptr.next = new ListNode(item);
-            ptr = ptr.next;
-        }
-        return dummyRoot.next;
-    }
-
-    public static String listNodeToString(ListNode node) {
-        if (node == null) {
-            return "[]";
-        }
-
-        String result = "";
-        while (node != null) {
-            result += Integer.toString(node.val) + ", ";
-            node = node.next;
-        }
-        return "[" + result.substring(0, result.length() - 2) + "]";
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String line;
-        while ((line = in.readLine()) != null) {
-            ListNode l1 = stringToListNode(line);
-            line = in.readLine();
-            ListNode l2 = stringToListNode(line);
-
-            ListNode ret = new Solution().addTwoNumbers(l1, l2);
-
-            String out = listNodeToString(ret);
-
-            System.out.print(out);
-        }
+    public static void main(String[] args){
+        String s = "pwwkew";
+        new Solution().lengthOfLongestSubstring(s);
     }
 }
