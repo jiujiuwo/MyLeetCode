cf79fee3295f4cfe172a7943458d928aa059d48b
jiujiuwo <1203574299@qq.com>
2018-11-19 14:44:01 +0800

    java 解决 5. leetcode Longest Palindromic Substring 超时

--- description.md
+++ description.md
@@ -226,5 +226,45 @@ func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
 	}
 	return median
 }
+```
 
+#### 2018/11/19 5. Longest Palindromic Substring
++ java 超时解，
+```java
+class Solution {
+    public String longestPalindrome(String s) {
+
+        String result = "";
+        String tmp ="";
+
+        for(int i=0;i< s.length();i++){
+            for (int j = s.length();j >=i;j--){
+                if(checkPalindrome(s.substring(i,j))){
+                    tmp = s.substring(i,j);
+                    if(tmp.length()>result.length()){
+                        result = tmp;
+                    }
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private boolean checkPalindrome(String substring) {
+      //  System.out.println(substring);
+
+        boolean isPalindrome = true;
+
+        for(int i= 0; i < substring.length()/2;i++){
+            if(substring.charAt(i)==substring.charAt(substring.length()-1-i)){
+                continue;
+            }else{
+                isPalindrome = false;
+            }
+        }
+
+        return isPalindrome;
+    }
+}
 ```
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,48 +1,42 @@
 class Solution {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+    public String longestPalindrome(String s) {
 
-        int len1 = nums1.length;
-        int len2 = nums2.length;
-        double median = 0;
-        int index = (len1+len2)/2;
+        String result = "";
+        String tmp ="";
 
-        int newNums[] = new int[index+1];
-
-        int m = 0,n = 0;
-        for(int i =0;i <= index;i++){
-            if(m  <nums1.length && n < nums2.length){
-                if(nums1[m] > nums2[n]){
-                    newNums[i] = nums2[n];
-                    n++;
-                }else{
-                    newNums[i] = nums1[m];
-                    m++;
+        for(int i=0;i< s.length();i++){
+            for (int j = s.length();j >=i;j--){
+                if(checkPalindrome(s.substring(i,j))){
+                    tmp = s.substring(i,j);
+                    if(tmp.length()>result.length()){
+                        result = tmp;
+                    }
                 }
+            }
+        }
+
+        return result;
+    }
+
+    private boolean checkPalindrome(String substring) {
+      //  System.out.println(substring);
+
+        boolean isPalindrome = true;
+
+        for(int i= 0; i < substring.length()/2;i++){
+            if(substring.charAt(i)==substring.charAt(substring.length()-1-i)){
                 continue;
-            }
-            if(m < nums1.length){
-                newNums[i] = nums1[m];
-                m++;
-            }
-            if(n < nums2.length){
-                newNums[i] = nums2[n];
-                n++;
+            }else{
+                isPalindrome = false;
             }
         }
 
-        if((nums1.length + nums2.length) % 2 == 0){
-            return (newNums[newNums.length-1] + newNums[newNums.length-2]) * 1.0 / 2;
-        }else {
-            return newNums[newNums.length-1];
-        }
-
+        return isPalindrome;
     }
 }
 public class MainClass {
     public static void main(String[] args){
-       int nums1[] ={1,3};
-       int nums2[] = {2,4};
-        double x = new Solution().findMedianSortedArrays(nums1,nums2);
+        String x = new Solution().longestPalindrome("babad");
         System.out.println(x);
     }
 }
