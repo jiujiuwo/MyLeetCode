0726f6a0129cde0f68ab7bbe6edf57b09a791c3d
jiujiuwo <1203574299@qq.com>
2018-11-21 22:34:25 +0800

    感觉做题没有一个清晰的思路，很不爽

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,41 +1,50 @@
 class Solution {
-    public String longestPalindrome(String s) {
+    public String convert(String s, int numRows) {
 
-        if (s == null || "".equals(s)) {
+        String result = "";
+        char[][] chars = new char[numRows][s.length()];
+        boolean down = true;
+
+        //一开始没有想到，行数为1的情况，导致编译错误。
+        if(numRows <=1){
             return s;
         }
 
-        int len = s.length();
+        //简单的题目做了一个多小时。。。惨
+        for(int i=0,j =0,index =0;index < s.length();index++){
+            chars[i][j] = s.charAt(index);
+           // System.out.println(i+" "+j);
+            if(down){
+                i++;
+            }else{
+                i--;
+                j++;
+            }
+           if(i==numRows){
+                i -= 2;     //边界条件搞不清楚，这里，一开始写的 i--,结果发现不对
+                j++;
+                down = false;
+           }
+           if(i==0){
+              // i++;           在向上走的时候，一直是i--,当i减到0时，
+               down = true;
+           }
+        }
 
-        String ans = "";
-        int max = 0;
-
-        boolean[][] dp = new boolean[len][len];
-
-        for (int j = 0; j < len; j++) {//为何先改变 j?
-
-            for (int i = 0; i <= j; i++) {
-                System.out.println(i+" "+j);
-
-                boolean judge = s.charAt(i) == s.charAt(j);
-
-                dp[i][j] = j - i > 2 ? dp[i + 1][j - 1] && judge : judge;
-
-                if (dp[i][j] && j - i + 1 > max) {
-                    max = j - i + 1;
-                    ans = s.substring(i, j + 1);
-                    System.out.println(max);
-
-                }
+        for(int i = 0;i < numRows;i++){
+            for(int j = 0; j < s.length();j++){
+                if(chars[i][j]!='\0')
+                result += chars[i][j];
             }
         }
-        return ans;
+
+        return result;
     }
 }
 public class MainClass {
     public static void main(String[] args){
         System.out.println(System.currentTimeMillis());
-        String x = new Solution().longestPalindrome("abba");
+        String x = new Solution().convert("PAYPALISHIRING",3);
         System.out.println(System.currentTimeMillis());
         System.out.println(x);
     }
