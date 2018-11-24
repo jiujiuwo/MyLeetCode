a9850d4f63e63e2efe9308f5a1bb23a911fa0dc2
jiujiuwo <1203574299@qq.com>
2018-11-24 21:50:22 +0800

    leetcode 第9题，判断数字是否是回文数字

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,73 +1,28 @@
 class Solution {
-    public int myAtoi(String str) {
+    /*
+        简单的操作，而且是一重循环，但是耗时较高，154ms.
+     */
+    public boolean isPalindrome(int x) {
 
-        double result = 0;
-        int figureCount = 0;    //处理 +0 123 这样的输入情况
-
-        //一开始没有判空
-        if(str.length()<=0){
-            return 0;
-        }
-
-        boolean isNegative = false;
-
-        for(int i= 0;i < str.length();i++){
-            //这里也要判断  figureCount==0  防止出现  0-1的情况
-            if(str.charAt(i)=='-' && result==0&&figureCount==0){
-                isNegative = true;
-                //防止出现正负号同时有的情况
-                if(i < str.length()-1 && str.charAt(i+1)>='0' && str.charAt(i+1)<='9'){
-                    continue;               //这里要加continue,否则，如果只有一个"-"号，会越界
-                }else {
-                    break;
-                }
-            }
-            if(str.charAt(i)=='+'&&result==0&&figureCount==0){
-                if(i < str.length()-1 && str.charAt(i+1)>='0' && str.charAt(i+1)<='9'){
-                    continue;               //这里要加continue,否则，如果只有一个"-"号，会越界
-                }else {
-                    break;
-                }
-            }
-            //System.out.println(i);
-            if(str.charAt(i)>='0' && str.charAt(i)<='9'){
-                result = result * 10 + str.charAt(i) - '0';
-                figureCount++;
-            }else if(str.charAt(i)==' '&&result==0&&figureCount==0){
+        String tmpX= x +"";
+        for(int i = 0;i <tmpX.length();i++){
+            if(tmpX.charAt(i)==tmpX.charAt(tmpX.length()-i-1)){
                 continue;
-            }else{
-                break;
+            }else {
+                return false;
             }
         }
 
-        //判断正负要在下面的代码之前
-       // System.out.println(result);
-
-        if(isNegative){
-            result = -result;
-        }
-
-       // System.out.println(result);
-        //System.out.println(Long.MAX_VALUE);
-
-        if(result >= Integer.MAX_VALUE||result>=Long.MAX_VALUE){
-            return Integer.MAX_VALUE;
-        }else if(result <= Integer.MIN_VALUE||result<=Long.MIN_VALUE){
-            return Integer.MIN_VALUE;
-        }
-
-        return (int)result;
+    return true;
     }
 }
+
+
 public class MainClass {
     public static void main(String[] args){
-      //  System.out.println(System.currentTimeMillis());
-        int x = new Solution().myAtoi("9223372036854775808");
-     //   System.out.println(System.currentTimeMillis());
+        boolean x = new Solution().isPalindrome(808);
+
         System.out.println(x);
-        //java 中 int 的最大值
-       // System.out.println(Integer.MAX_VALUE);
-      //  System.out.println(Long.MAX_VALUE);
-        //System.out.println(Math.pow(2,31)-1);
+
     }
 }
