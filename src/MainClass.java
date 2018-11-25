2fac65cb724a8457e9f291e6f4e0d7c3138584c3
jiujiuwo <1203574299@qq.com>
2018-11-25 11:51:49 +0800

    leetcode 第10题，使用java内置正则表达式方法的解法，速度较慢

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,26 +1,15 @@
 class Solution {
-    /*
-        简单的操作，而且是一重循环，但是耗时较高，154ms.
-     */
-    public boolean isPalindrome(int x) {
+    public boolean isMatch(String s, String p) {
 
-        String tmpX= x +"";
-        for(int i = 0;i <tmpX.length();i++){
-            if(tmpX.charAt(i)==tmpX.charAt(tmpX.length()-i-1)){
-                continue;
-            }else {
-                return false;
-            }
-        }
+        boolean isMatch = s.matches(p);
 
-    return true;
+        return isMatch;
     }
 }
 
-
 public class MainClass {
     public static void main(String[] args){
-        boolean x = new Solution().isPalindrome(808);
+        boolean x = new Solution().isMatch("aa","a");
 
         System.out.println(x);
 
