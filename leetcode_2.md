a6ec05378d34524c5c1ceca511a8d711e7325341
jiujiuwo <1203574299@qq.com>
2018-12-02 18:08:35 +0800

    leetcode 12 题解法

--- leetcode_2.md
+++ leetcode_2.md
@@ -122,3 +122,105 @@ func maxArea(height []int) int {
 }
 
 ```
+
+## 12. Integer to Roman
+
++ java 分类讨论思路，获取每一位上的数字做转换，代码量较大
++ Runtime: 75 ms, faster than 20.56% of Java online submissions for Integer to Roman.
+```java
+class Solution {
+
+    private String add(String tmp,int count){
+        if(count <= 3){
+            for(int i = 0;i < count;i++){
+                tmp +="I";
+            }
+        }else if(count == 4){
+            tmp +="IV";
+        }else if(count < 9 && count >4){
+            tmp +="V";
+            for(int i = 0;i < count-5;i++){
+                tmp +="I";
+            }
+        }else {
+            tmp += "IX";
+        }
+        return tmp;
+    }
+
+    private String add10(String tmp,int count){
+        if(count <= 3){
+            for(int i = 0; i < count ;i++){
+                tmp +="X";
+            }
+        }else if(count == 4){
+            tmp += "XL";
+        }else if(count < 9 && count >4){
+            tmp +="L";
+            for(int i = 0;i < count-5;i++){
+                tmp +="X";
+            }
+        }else{
+            tmp += "XC";
+        }
+        return tmp;
+    }
+    private String add100(String tmp,int count){
+        if(count <= 3){
+            for(int i = 0; i < count ;i++){
+                tmp +="C";
+            }
+
+        }else if(count == 4){
+            tmp += "CD";
+        }else if(count < 9 && count >4){
+            tmp +="D";
+            for(int i = 0;i < count-5;i++){
+                tmp +="C";
+            }
+        }else{
+            tmp += "CM";
+        }
+        return tmp;
+    }
+
+    private String add1000(String tmp,int count){
+        if(count <= 3){
+            for(int i = 0; i < count ;i++){
+                tmp +="M";
+            }
+        }else {
+            return "error";
+        }
+        return tmp;
+    }
+
+    public String intToRoman(int num) {
+
+        String result = "";
+        String numS = (num+"");
+
+        if(num >= 1000){
+            result = add1000(result,numS.charAt(0)-'0');
+          //  System.out.println(result);
+            result = add100(result,numS.charAt(1)-'0');
+          //  System.out.println(result);
+            result = add10(result,numS.charAt(2)-'0');
+          //  System.out.println(result);
+            result = add(result,numS.charAt(3)-'0');
+            //System.out.println(result);
+        }else if(num <1000 && num>=100){
+            result = add100(result,numS.charAt(0)-'0');
+            result = add10(result,numS.charAt(1)-'0');
+            result = add(result,numS.charAt(2)-'0');
+        }else if(num<100 && num>=10){
+            result = add10(result,numS.charAt(0)-'0');
+            result = add(result,numS.charAt(1)-'0');
+        }else{
+            result = add(result,numS.charAt(0)-'0');
+        }
+        return result;
+    }
+
+}
+```
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,28 +1,105 @@
 class Solution {
-    //O(n)的解法，因为左右取两者小后是对称的，所以，没有必要二重循环，
-    public int maxArea(int[] height) {
-        int max = 0,s = 0;
 
-        for(int i = 0,j = height.length-1; i < j;){
-            if(height[i] > height[j]) {
-                s = height[j]*(j-i);
-                j--;
+    private String add(String tmp,int count){
+        if(count <= 3){
+            for(int i = 0;i < count;i++){
+                tmp +="I";
             }
-            else {
-                s = height[i]*(j-i);
-                i++;
+        }else if(count == 4){
+            tmp +="IV";
+        }else if(count < 9 && count >4){
+            tmp +="V";
+            for(int i = 0;i < count-5;i++){
+                tmp +="I";
             }
-            if(s > max) max = s;
+        }else {
+            tmp += "IX";
         }
-
-        return max;
+        return tmp;
     }
+
+    private String add10(String tmp,int count){
+        if(count <= 3){
+            for(int i = 0; i < count ;i++){
+                tmp +="X";
+            }
+        }else if(count == 4){
+            tmp += "XL";
+        }else if(count < 9 && count >4){
+            tmp +="L";
+            for(int i = 0;i < count-5;i++){
+                tmp +="X";
+            }
+        }else{
+            tmp += "XC";
+        }
+        return tmp;
+    }
+    private String add100(String tmp,int count){
+        if(count <= 3){
+            for(int i = 0; i < count ;i++){
+                tmp +="C";
+            }
+
+        }else if(count == 4){
+            tmp += "CD";
+        }else if(count < 9 && count >4){
+            tmp +="D";
+            for(int i = 0;i < count-5;i++){
+                tmp +="C";
+            }
+        }else{
+            tmp += "CM";
+        }
+        return tmp;
+    }
+
+    private String add1000(String tmp,int count){
+        if(count <= 3){
+            for(int i = 0; i < count ;i++){
+                tmp +="M";
+            }
+        }else {
+            return "error";
+        }
+        return tmp;
+    }
+
+    public String intToRoman(int num) {
+
+        String result = "";
+        String numS = (num+"");
+
+        if(num >= 1000){
+            result = add1000(result,numS.charAt(0)-'0');
+          //  System.out.println(result);
+            result = add100(result,numS.charAt(1)-'0');
+          //  System.out.println(result);
+
+            result = add10(result,numS.charAt(2)-'0');
+          //  System.out.println(result);
+
+            result = add(result,numS.charAt(3)-'0');
+            //System.out.println(result);
+
+        }else if(num <1000 && num>=100){
+            result = add100(result,numS.charAt(0)-'0');
+            result = add10(result,numS.charAt(1)-'0');
+            result = add(result,numS.charAt(2)-'0');
+        }else if(num<100 && num>=10){
+            result = add10(result,numS.charAt(0)-'0');
+            result = add(result,numS.charAt(1)-'0');
+        }else{
+            result = add(result,numS.charAt(0)-'0');
+        }
+        return result;
+    }
+
 }
 public class MainClass {
     public static void main(String[] args){
         int[] tmp = {1,8,6,2,5,4,8,3,7};
-        int x = new Solution().maxArea(tmp);
+        String x = new Solution().intToRoman(58);
         System.out.println(x);
-
     }
 }
