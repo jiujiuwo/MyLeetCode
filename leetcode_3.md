bdd8f7f5f31e745556c8789c62cbf4c4baebe800
jiujiuwo <1203574299@qq.com>
2018-12-09 00:11:53 +0800

    leetcode第21题，合并两个有序的链表

---
+++ leetcode_3.md
@@ -0,0 +1,39 @@
+## 21. Merge Two Sorted Lists
++ java解法
++ Runtime: 8 ms, faster than 74.95% of Java online submissions for Merge Two Sorted Lists.
+```java
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode ptr1 = l1;
+        ListNode ptr2 = l2;
+        ListNode head = new ListNode(0);
+        ListNode ptr = head;
+
+        while (ptr1!=null||ptr2!=null){
+            if(ptr1!=null&&ptr2!=null){
+                if(ptr1.val>ptr2.val){
+                    ptr.next = new ListNode(ptr2.val);
+                    ptr2 = ptr2.next;
+                    ptr = ptr.next;
+                }else{
+                    ptr.next = new ListNode(ptr1.val);
+                    ptr1 = ptr1.next;
+                    ptr = ptr.next;
+                }
+            }else if(ptr1!=null&&ptr2==null){
+                ptr.next = new ListNode(ptr1.val);
+                ptr1 = ptr1.next;
+                ptr = ptr.next;
+            }else if(ptr1==null&&ptr2!=null){
+                ptr.next = new ListNode(ptr2.val);
+                ptr2 = ptr2.next;
+                ptr = ptr.next;
+            }
+        }
+        head = head.next;
+
+    return head;
+    }
+}
+```
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,27 +1,36 @@
-import java.util.Stack;
 
 class Solution {
-    public boolean isValid(String s) {
-        Stack<Character> stack = new Stack<>();
-        for(int i=0;i < s.length();i++){
-            char tmp = s.charAt(i);
-            if(tmp == '('||tmp=='['||tmp=='{'){
-                stack.push(tmp);
-            }else if(tmp ==')'){
-                if(stack.isEmpty()||stack.pop()!='('){
-                    return false;
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode ptr1 = l1;
+        ListNode ptr2 = l2;
+        ListNode head = new ListNode(0);
+        ListNode ptr = head;
+
+        while (ptr1!=null||ptr2!=null){
+            if(ptr1!=null&&ptr2!=null){
+                if(ptr1.val>ptr2.val){
+                    ptr.next = new ListNode(ptr2.val);
+                    ptr2 = ptr2.next;
+                    ptr = ptr.next;
+                }else{
+                    ptr.next = new ListNode(ptr1.val);
+                    ptr1 = ptr1.next;
+                    ptr = ptr.next;
                 }
-            }else if(tmp =='}'){
-                if(stack.isEmpty()||stack.pop()!='{'){
-                    return false;
-                }
-            }else if(tmp ==']'){
-                if(stack.isEmpty()||stack.pop()!='['){
-                    return false;
-                }
+            }else if(ptr1!=null&&ptr2==null){
+                ptr.next = new ListNode(ptr1.val);
+                ptr1 = ptr1.next;
+                ptr = ptr.next;
+            }else if(ptr1==null&&ptr2!=null){
+                ptr.next = new ListNode(ptr2.val);
+                ptr2 = ptr2.next;
+                ptr = ptr.next;
             }
         }
-        return stack.isEmpty();
+        head = head.next;
+
+    return head;
     }
 }
 
