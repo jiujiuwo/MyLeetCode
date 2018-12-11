8b9ad0fd6cb77497a66e15ceb92eb308982854d4
jiujiuwo <1203574299@qq.com>
2018-12-11 22:16:59 +0800

    未做完的25题

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,52 +1,30 @@
 class Solution {
+    public ListNode reverseKGroup(ListNode head, int k) {
 
-    public ListNode mergeKLists(ListNode[] lists) {
-        ListNode newList = null;
+        ListNode headPtr = head;
 
-        if(lists.length==1){
-            return lists[0];
+        while (headPtr!=null){
+            headPtr = reverseGroup(headPtr,k);
+            headPtr = headPtr.next;
         }
 
-        for(int i=0;i < lists.length-1;i++){
-            newList = mergeTwoLists(lists[i],lists[i+1]);
-            if(i+1 < lists.length){
-                lists[i+1] = newList;
-            }
-        }
-        return newList;
+        return null;
     }
 
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-
-        ListNode ptr1 = l1;
-        ListNode ptr2 = l2;
-        ListNode head = new ListNode(0);
-        ListNode ptr = head;
-
-        while (ptr1!=null||ptr2!=null){
-            if(ptr1!=null&&ptr2!=null){
-                if(ptr1.val>ptr2.val){
-                    ptr.next = new ListNode(ptr2.val);
-                    ptr2 = ptr2.next;
-                    ptr = ptr.next;
-                }else{
-                    ptr.next = new ListNode(ptr1.val);
-                    ptr1 = ptr1.next;
-                    ptr = ptr.next;
-                }
-            }else if(ptr1!=null&&ptr2==null){
-                ptr.next = new ListNode(ptr1.val);
-                ptr1 = ptr1.next;
-                ptr = ptr.next;
-            }else if(ptr1==null&&ptr2!=null){
-                ptr.next = new ListNode(ptr2.val);
-                ptr2 = ptr2.next;
-                ptr = ptr.next;
+    private ListNode reverseGroup(ListNode headPtr, int k) {
+        ListNode tmpHead = headPtr;
+        int index=0;
+        while (tmpHead!=null){
+            index++;
+            tmpHead = tmpHead.next;
+            if(index==k){
+                break;
             }
         }
-        head = head.next;
-        return head;
+        return tmpHead;
     }
+
+
 }
 public class MainClass {
     public static void main(String[] args){
