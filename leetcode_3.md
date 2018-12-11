684fadfbd76db1d52e4c02188005f7e5b3d3dc2b
jiujiuwo <1203574299@qq.com>
2018-12-11 21:07:29 +0800

    leetcode 27题，普通做法，交换元素

--- leetcode_3.md
+++ leetcode_3.md
@@ -277,3 +277,35 @@ class Solution {
     }
 }
 ```
+
+## 27. Remove Element
++ java,O(n) 普通做法，题目虽然简单，但是需要注意的地方很多。详细见注释
++ Runtime: 8 ms, faster than 19.18% of Java online submissions for Remove Element.
+```java
+class Solution {
+    public int removeElement(int[] nums, int val) {
+
+        if(nums.length==0){
+            return 0;
+        }
+
+        int count = 0;
+        //一开始 i<j,导致如果输入[4,5]的话，因为i<j所以 i不会等于1，导致错误
+        for(int i=0,j=nums.length-1;i<=j;i++){
+            if(nums[i] == val){
+                int tmp = nums[i];
+                //一开始缺少条件j<i导致越界错误
+                while(j>i&&nums[j]==val){
+                    count++;
+                    j--;
+                }
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                j--;
+                count++;
+            }
+        }
+        return nums.length-count;
+    }
+}
+```
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,31 +1,42 @@
 class Solution {
-    public ListNode swapPairs(ListNode head) {
+    public int removeElement(int[] nums, int val) {
 
-        if(head==null){
-            return head;
+        if(nums.length==0){
+            return 0;
         }
-
-        ListNode ptr=head;
-        int index =0;
-        while (ptr!=null){
-            if(index % 2 == 0){
-                if(ptr.next!=null){
-                    int tmp = ptr.val;
-                    ptr.val = ptr.next.val;
-                    ptr.next.val = tmp;
-                }
-                ptr = ptr.next;
+        //这部分在i=j后就不需要了
+/*
+        if(nums.length==1){
+            if(nums[0]==val){
+                return 0;
             }else{
-                ptr = ptr.next;
+                return 1;
             }
-            index++;
         }
+*/
 
-        return null;
+        int count = 0;
+        //一开始 i<j,导致如果输入[4,5]的话，因为i<j所以 i不会等于1，导致错误
+        for(int i=0,j=nums.length-1;i<=j;i++){
+            if(nums[i] == val){
+                int tmp = nums[i];
+                //一开始缺少条件j<i导致越界错误
+                while(j>i&&nums[j]==val){
+                    count++;
+                    j--;
+                }
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                j--;
+                count++;
+            }
+        }
+        return nums.length-count;
     }
 }
 public class MainClass {
     public static void main(String[] args){
-
+        int[] nums = {4,5};
+        System.out.println(new Solution().removeElement(nums,5));
     }
 }
