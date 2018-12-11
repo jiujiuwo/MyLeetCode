8808f7645a8f0972725f72223e0dd19e0dcbef25
jiujiuwo <1203574299@qq.com>
2018-12-11 21:58:46 +0800

    23. Merge k Sorted Lists 第二种做法，使用merge two sorted list,超时

--- leetcode_3.md
+++ leetcode_3.md
@@ -145,7 +145,7 @@ class Solution {
     }
 }
 ```
-## 23. Merge k Sorted Lists
+## 23. Merge k Sorted Lists ****
 + java,最简单的做法，先收集数字，然后排序，然后构建结果
 + Your runtime beats 24.69 % of java submissions
 ```java
@@ -187,6 +187,63 @@ class Solution {
     }
 }
 ```
++ java,0(N<sup>2</sup>)做法，多次调用mergeTwoList
++ 超时,130 / 131 test cases passed.
+```java
+class Solution {
+
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode newList = null;
+
+        if(lists.length==1){
+            return lists[0];
+        }
+
+        for(int i=0;i < lists.length-1;i++){
+            newList = mergeTwoLists(lists[i],lists[i+1]);
+            if(i+1 < lists.length){
+                lists[i+1] = newList;
+            }
+        }
+        return newList;
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode ptr1 = l1;
+        ListNode ptr2 = l2;
+        ListNode head = new ListNode(0);
+        ListNode ptr = head;
+
+        while (ptr1!=null||ptr2!=null){
+            if(ptr1!=null&&ptr2!=null){
+                if(ptr1.val>ptr2.val){
+                    ptr.next = new ListNode(ptr2.val);
+                    ptr2 = ptr2.next;
+                    ptr = ptr.next;
+                }else{
+                    ptr.next = new ListNode(ptr1.val);
+                    ptr1 = ptr1.next;
+                    ptr = ptr.next;
+                }
+            }else if(ptr1!=null&&ptr2==null){
+                ptr.next = new ListNode(ptr1.val);
+                ptr1 = ptr1.next;
+                ptr = ptr.next;
+            }else if(ptr1==null&&ptr2!=null){
+                ptr.next = new ListNode(ptr2.val);
+                ptr2 = ptr2.next;
+                ptr = ptr.next;
+            }
+        }
+        head = head.next;
+
+        return head;
+    }
+}
+```
+#### 后续尝试更多的做法
+
 ## 24. Swap Nodes in Pairs
 + java,按照下标的奇偶来交换元素
 + Runtime: 2 ms, faster than 99.98% of Java online submissions for Swap Nodes in Pairs.
--- src/MainClass.java
+++ src/MainClass.java
@@ -1,19 +1,56 @@
 class Solution {
-    public int strStr(String haystack, String needle) {
 
-        //注意这里是 <=
-        for(int i=0;i<=haystack.length()-needle.length();i++){
-            //System.out.println(haystack.substring(i,i+needle.length()));
-            if(haystack.substring(i,i+needle.length()).equals(needle)){
-                return i;
+    public ListNode mergeKLists(ListNode[] lists) {
+        ListNode newList = null;
+
+        if(lists.length==1){
+            return lists[0];
+        }
+
+        for(int i=0;i < lists.length-1;i++){
+            newList = mergeTwoLists(lists[i],lists[i+1]);
+            if(i+1 < lists.length){
+                lists[i+1] = newList;
             }
         }
-        return -1;
+        return newList;
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        ListNode ptr1 = l1;
+        ListNode ptr2 = l2;
+        ListNode head = new ListNode(0);
+        ListNode ptr = head;
+
+        while (ptr1!=null||ptr2!=null){
+            if(ptr1!=null&&ptr2!=null){
+                if(ptr1.val>ptr2.val){
+                    ptr.next = new ListNode(ptr2.val);
+                    ptr2 = ptr2.next;
+                    ptr = ptr.next;
+                }else{
+                    ptr.next = new ListNode(ptr1.val);
+                    ptr1 = ptr1.next;
+                    ptr = ptr.next;
+                }
+            }else if(ptr1!=null&&ptr2==null){
+                ptr.next = new ListNode(ptr1.val);
+                ptr1 = ptr1.next;
+                ptr = ptr.next;
+            }else if(ptr1==null&&ptr2!=null){
+                ptr.next = new ListNode(ptr2.val);
+                ptr2 = ptr2.next;
+                ptr = ptr.next;
+            }
+        }
+        head = head.next;
+        return head;
     }
 }
 public class MainClass {
     public static void main(String[] args){
         int[] nums = {3,2,2,3};
-        System.out.println(new Solution().strStr("hello","ll"));
+       // System.out.println(new Solution().mergeKLists("hello","ll"));
     }
 }
