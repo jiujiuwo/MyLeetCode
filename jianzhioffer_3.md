333577eebe7a76f2367ecab7a326001376ffc7b6
jiujiuwo <l>
2019-06-19 03:20:52 +0800

    剑指offer 29题，最小k个数

--- jianzhioffer_3.md
+++ jianzhioffer_3.md
@@ -739,5 +739,63 @@ public class Solution {
 }
 ```
 # 29. 最小的K个数
++ 题目描述
+```
+输入n个整数，找出其中最小的K个数。例如输入4,5,1,6,2,7,3,8这8个数字，则最小的4个数字是1,2,3,4,。
+```
++ 解法1 ，使用快速排序
+```java
+import java.util.ArrayList;
 
+public class Solution {
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        //如果k > 输入数组的长度。返回所有元素还是返回空？
+        if(k>input.length){
+            return result;
+        }
+
+        quickSort(input,0,input.length-1);
+
+        for(int i=0;i<k;i++){
+            result.add(input[i]);
+        }
+
+        return result;
+    }
+
+    //快速排序
+    static void quickSort(int[] input,int left,int right){
+        if(left>=right){
+            return;
+        }
+
+        int p = input[left];
+
+        int i = left;
+        int j = right;
+
+        while (i<j){
+            while(input[j]>=p&&i<j){
+                j--;
+            }
+            while(input[i]<=p&&i<j){
+                i++;
+            }
+
+            if(i<j){
+                int tmp = input[i];
+                input[i] = input[j];
+                input[j] = tmp;
+            }
+        }
+
+        input[left] = input[i];
+        input[i] = p;
+        quickSort(input,left,i-1);
+        quickSort(input,i+1,right);
+    }
+}
+```
 # 30. 连续子数组的最大和
--- src/Solution.java
+++ src/Solution.java
@@ -1,21 +1,52 @@
 import java.util.ArrayList;
-import java.util.List;
 
 public class Solution {
-    public boolean IsBalanced_Solution(TreeNode root) {
-        List<TreeNode> tmp = new ArrayList<>();
-        tmp.add(root);
-        for(int i=0;i<tmp.size();i++){
-            int deep = 0;
+    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
+        ArrayList<Integer> result = new ArrayList<>();
 
-            if(root.left!=null){
-                tmp.add(root.left);
-            }
-            if(root.right!=null){
-                tmp.add(root.right);
-            }
-            deep++;
+        //如果k > 输入数组的长度。返回所有元素还是返回空？
+        if(k>input.length){
+            return result;
         }
-        return true;
+
+        quickSort(input,0,input.length-1);
+
+        for(int i=0;i<k;i++){
+            result.add(input[i]);
+        }
+
+        return result;
+    }
+
+    //快速排序
+    static void quickSort(int[] input,int left,int right){
+        if(left>=right){
+            return;
+        }
+
+        int p = input[left];
+
+        int i = left;
+        int j = right;
+
+        while (i<j){
+            while(input[j]>=p&&i<j){
+                j--;
+            }
+            while(input[i]<=p&&i<j){
+                i++;
+            }
+
+            if(i<j){
+                int tmp = input[i];
+                input[i] = input[j];
+                input[j] = tmp;
+            }
+        }
+
+        input[left] = input[i];
+        input[i] = p;
+        quickSort(input,left,i-1);
+        quickSort(input,i+1,right);
     }
 }
