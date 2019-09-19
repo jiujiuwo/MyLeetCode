969885c14297e4a15dae036b51ffc63d683326ae
jiujiuwo <l>
2019-09-19 11:41:29 +0800

    最大连续子序列的和

--- src/Solution.java
+++ src/Solution.java
@@ -1,52 +1,19 @@
-import java.util.ArrayList;
-
 public class Solution {
-    public ArrayList<Integer> GetLeastNumbers_Solution(int [] input, int k) {
-        ArrayList<Integer> result = new ArrayList<>();
+    public int FindGreatestSumOfSubArray(int[] array) {
+        int result = Integer.MIN_VALUE;
 
-        //如果k > 输入数组的长度。返回所有元素还是返回空？
-        if(k>input.length){
-            return result;
-        }
-
-        quickSort(input,0,input.length-1);
-
-        for(int i=0;i<k;i++){
-            result.add(input[i]);
+        for(int i=0;i<array.length;i++){
+            for(int j=i;j<array.length;j++){
+                int sum = 0;
+                for(int k=i;k<j;k++){
+                    sum+=array[i];
+                }
+                if(sum>result){
+                    result = sum;
+                }
+            }
         }
 
         return result;
-    }
-
-    //快速排序
-    static void quickSort(int[] input,int left,int right){
-        if(left>=right){
-            return;
-        }
-
-        int p = input[left];
-
-        int i = left;
-        int j = right;
-
-        while (i<j){
-            while(input[j]>=p&&i<j){
-                j--;
-            }
-            while(input[i]<=p&&i<j){
-                i++;
-            }
-
-            if(i<j){
-                int tmp = input[i];
-                input[i] = input[j];
-                input[j] = tmp;
-            }
-        }
-
-        input[left] = input[i];
-        input[i] = p;
-        quickSort(input,left,i-1);
-        quickSort(input,i+1,right);
     }
 }
